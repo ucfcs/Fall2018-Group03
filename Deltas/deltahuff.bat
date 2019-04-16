8c0320eff850e6b581aa22341e09b24b9fbe94f3
warmCabin <35278529+warmCabin@users.noreply.github.com>
2019-04-16 14:48:37 -0400

    Added batch files for chaining lzΔ & Huffman
    They almost certainly do not work on your computer.

---
+++ Deltas/deltahuff.bat
@@ -0,0 +1,2 @@
+lzss_deltas -c -i %1 -o temp
+java -cp "C:\Users\ry791283\Desktop\Isaac's Funhouse\Reference-Huffman-coding-master\java\src" HuffmanCompress temp %2
---
+++ Deltas/lzhuff.bat
@@ -0,0 +1,2 @@
+lzss -c -i %1 -o temp
+java -cp "C:\Users\ry791283\Desktop\Isaac's Funhouse\Reference-Huffman-coding-master\java\src" HuffmanCompress temp %2
