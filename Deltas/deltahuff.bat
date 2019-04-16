b3301818882c6c9b81c2b01aaaa8a8db77e2b5f8
warmCabin <35278529+warmCabin@users.noreply.github.com>
2019-04-16 16:27:56 -0400

    Add files via upload

--- Deltas/deltahuff.bat
+++ Deltas/deltahuff.bat
@@ -1,2 +1,4 @@
+Pushd "%~dp0"
 lzss_deltas -c -i %1 -o temp
-java -cp "C:\Users\ry791283\Desktop\Isaac's Funhouse\Reference-Huffman-coding-master\java\src" HuffmanCompress temp %2
+java -cp "Reference-Huffman-coding-master\java\src" HuffmanCompress temp %2
+popd
--- Deltas/lzhuff.bat
+++ Deltas/lzhuff.bat
@@ -1,2 +1,4 @@
+Pushd "%~dp0"
 lzss -c -i %1 -o temp
-java -cp "C:\Users\ry791283\Desktop\Isaac's Funhouse\Reference-Huffman-coding-master\java\src" HuffmanCompress temp %2
+java -cp "Reference-Huffman-coding-master\java\src" HuffmanCompress temp %2
+popd
