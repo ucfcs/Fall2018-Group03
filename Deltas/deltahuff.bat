f7dccdff65c491908c5a7f850dff8b39da670ac7
Jay Brown <jbrown32597@gmail.com>
2019-04-23 11:40:38 -0400

    Final cleanup Part 2

--- Deltas/deltahuff.bat
+++
@@ -1,5 +0,0 @@
-pushd "%~dp0"
-del /f /q %2.tmp
-lzss_deltas -c -i %1 -o %2.tmp
-java -cp C:\Users\Ryan\workspace\HuffmanStuffman\bin HuffmanCompress %2.tmp %2
-popd
--- Deltas/lzhuff.bat
+++
@@ -1,5 +0,0 @@
-pushd "%~dp0"
-del /f /q %2.temp
-lzss -c -i %1 -o %2.temp
-java -cp C:\Users\Ryan\workspace\HuffmanStuffman\bin HuffmanCompress %2.temp %2
-popd
